a198b02d7c2dd52e75b67de2ea248f0798eee291
Bernardo Biesseck <biesseck@gmail.com>
2022-11-04 20:47:25 -0300

    Remove commented code

--- models/pointnet2_verif_pairs_ssg.py
+++ models/pointnet2_verif_pairs_ssg.py
@@ -16,55 +16,12 @@ from pointnet_util_verif import pointnet_sa_module
 
 
 
-# from tensorflow.keras import applications
-# from tensorflow.keras import layers
-# from tensorflow.keras import losses
-# from tensorflow.keras import optimizers
-# from tensorflow.keras import metrics
-# from tensorflow.keras import Model
-
-'''  # original reference
-def placeholder_inputs(batch_size, num_point):
-    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
-    labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
-    return pointclouds_pl, labels_pl
-'''
-
 # Bernardo
 def placeholder_inputs(batch_size, num_point):
     pointclouds_pl1 = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
     pointclouds_pl2 = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
     labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
     return pointclouds_pl1, pointclouds_pl2, labels_pl
-
-''' # original reference
-def get_model(point_cloud, is_training, bn_decay=None, num_class=40):
-    """ Classification PointNet, input is BxNx3, output Bx40 """
-    batch_size = point_cloud.get_shape()[0].value
-    num_point = point_cloud.get_shape()[1].value
-    end_points = {}
-    l0_xyz = point_cloud
-    l0_points = None
-    end_points['l0_xyz'] = l0_xyz
-
-    # Set abstraction layers
-    # Note: When using NCHW for layer 2, we see increased GPU memory usage (in TF1.4).
-    # So we only use NCHW for layer 1 until this issue can be resolved.
-    l1_xyz, l1_points, l1_indices = pointnet_sa_module(l0_xyz, l0_points, npoint=512, radius=0.2, nsample=32, mlp=[64,64,128], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer1', use_nchw=True)
-    l2_xyz, l2_points, l2_indices = pointnet_sa_module(l1_xyz, l1_points, npoint=128, radius=0.4, nsample=64, mlp=[128,128,256], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer2')
-    l3_xyz, l3_points, l3_indices = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3')
-
-    # Fully connected layers
-    net = tf.reshape(l3_points, [batch_size, -1])
-    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
-    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp1')
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
-    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp2')
-    # net = tf_util.fully_connected(net, 40, activation_fn=None, scope='fc3')        # original
-    net = tf_util.fully_connected(net, num_class, activation_fn=None, scope='fc3')   # Bernardo
-
-    return net, end_points
-'''
 
 
 # Bernardo
