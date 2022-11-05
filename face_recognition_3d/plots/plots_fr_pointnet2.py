c030f0239df07377fa1ec8c853021258048823ef
Bernardo Biesseck <biesseck@gmail.com>
2022-11-05 16:33:51 -0300

    face_recognition_3d/plots/plots_fr_pointnet2.py: adjusts on loading training data from 'log_train.txt' file

--- face_recognition_3d/plots/plots_fr_pointnet2.py
+++ face_recognition_3d/plots/plots_fr_pointnet2.py
@@ -44,6 +44,29 @@ def load_original_training_log_pointnet2(path_file=''):
         return parameters, epoch, eval_mean_loss, eval_accuracy, eval_avg_class_acc
 
 
+def load_original_training_log_pointnet2_verif_pairs(path_file=''):
+    with open(path_file) as f:
+        all_lines = f.readlines()
+        parameters, epoch, eval_mean_loss, eval_accuracy = [], [], [], []
+        for i, line in enumerate(all_lines):
+            line = line[:-1]
+            # print('line:', line)
+            if line.startswith('Namespace'):
+                # parameters = line.replace('Namespace', '').replace('(', '').replace(')', '')
+                parameters = line.replace('Namespace', '')
+                # print('parameters:', parameters)
+            if line.startswith('---- EPOCH') and line.endswith('EVALUATION ----'):
+                epoch.append(int(line.split(' ')[2]))
+                if i+1 < len(all_lines) and all_lines[i+1].startswith('eval mean loss:'):
+                    eval_mean_loss.append(float(all_lines[i+1][:-1].split(':')[-1]))
+                if i+2 < len(all_lines) and all_lines[i+2].startswith('eval accuracy:'):
+                    eval_accuracy.append(float(all_lines[i+2][:-1].split(':')[-1]))
+        epoch = np.array(epoch, dtype=np.int32)
+        eval_mean_loss = np.array(eval_mean_loss, dtype=np.float32)
+        eval_accuracy = np.array(eval_accuracy, dtype=np.float32)
+        return parameters, epoch, eval_mean_loss, eval_accuracy
+
+
 def plot_training_history_pointnet2(epoch, eval_mean_loss, eval_accuracy, eval_avg_class_acc, title='', subtitle='', path_image='.', show_fig=False, save_fig=False):
     # plot loss during training
     pyplot.clf()
@@ -73,6 +96,48 @@ def plot_training_history_pointnet2(epoch, eval_mean_loss, eval_accuracy, eval_a
     
     if save_fig:
         pyplot.savefig(path_image)
+        if path_image.endswith('.png'):
+            pyplot.savefig(path_image.replace('.png', '.svg'))
+        if path_image.endswith('.jpg'):
+            pyplot.savefig(path_image.replace('.jpg', '.svg'))
+    if show_fig:
+        pyplot.show()
+
+
+def plot_training_history_pointnet2_verif_pairs(epoch, eval_mean_loss, eval_accuracy, title='', subtitle='', path_image='.', show_fig=False, save_fig=False):
+    # plot loss during training
+    pyplot.clf()
+    # eval_accuracy = eval_mean_loss.copy()
+    if len(eval_mean_loss) > 0:
+        if len(eval_accuracy) > 0:
+            pyplot.subplot(211)
+        pyplot.plot(epoch, eval_mean_loss, label='eval_mean_loss', color='red')
+        pyplot.xlabel('Epoch')
+        pyplot.ylabel('Error')
+        pyplot.ylim(0, np.max(eval_mean_loss)*1.25)
+        pyplot.legend()
+    
+    pyplot.suptitle(title, fontsize=11, fontweight='bold')
+    pyplot.title(subtitle, fontsize=8)
+    
+    if len(eval_accuracy) > 0:
+        # plot accuracy during training
+        pyplot.subplot(212)
+        # pyplot.title('Accuracy')
+        pyplot.plot(epoch, eval_accuracy, label='eval_accuracy', color='blue')
+        pyplot.xlabel('Epoch')
+        pyplot.ylabel('Accuracy')
+        pyplot.ylim(0, 1)
+        pyplot.legend()
+
+    pyplot.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.75, wspace=0.4, hspace=0.4)
+
+    if save_fig:
+        pyplot.savefig(path_image)
+        if path_image.endswith('.png'):
+            pyplot.savefig(path_image.replace('.png', '.svg'))
+        if path_image.endswith('.jpg'):
+            pyplot.savefig(path_image.replace('.jpg', '.svg'))
     if show_fig:
         pyplot.show()
 
@@ -163,18 +228,19 @@ if __name__ == '__main__':
         # sys.argv += ['-input_path', '/home/bjgbiesseck/GitHub/pointnet2_tf_original_biesseck/face_recognition_3d/logs_training/log_face_recognition_2022-10-21_FGRCv2_dataset_133classes_lr=0.005/log_train.txt']
         # sys.argv += ['-input_path', '/home/bjgbiesseck/GitHub/pointnet2_tf_original_biesseck/face_recognition_3d/logs_training/log_face_recognition_2022-10-21_FGRCv2_dataset_133classes_lr=0.01/log_train.txt']
         # sys.argv += ['-input_path', '/home/bjgbiesseck/GitHub/pointnet2_tf_original_biesseck/face_recognition_3d/logs_training/log_face_recognition_2022-10-24_SyntheticFaces_dataset_100classes_10exp_lr=0.001_batch=32/log_train.txt']
-        sys.argv += ['-input_path', '/home/bjgbiesseck/GitHub/pointnet2_tf_original_biesseck/face_recognition_3d/logs_training/log_face_recognition_2022-10-27_LFW-3D-MICA-dataset_whole-head_minsamples=3_lr=0.001_batch=64/log_train.txt']
+        sys.argv += ['-input_path', '/home/bjgbiesseck/GitHub/pointnet2_tf_original_biesseck/face_recognition_3d/logs_training/log_face_recognition/log_train.txt']
 
 
     args = parse_args()
 
     # load_original_training_log_pointnet2(path_file=args.input_path)
 
-    parameters, epoch, eval_mean_loss, eval_accuracy, eval_avg_class_acc = load_original_training_log_pointnet2(path_file=args.input_path)
+    # parameters, epoch, eval_mean_loss, eval_accuracy, eval_avg_class_acc = load_original_training_log_pointnet2(path_file=args.input_path)
+    parameters, epoch, eval_mean_loss, eval_accuracy = load_original_training_log_pointnet2_verif_pairs(path_file=args.input_path)
     
-    title = 'PointNet++ training on LFW-Reconst3D-MICA \nClassification (1:N) - 901 classes - min 3 samples'
+    title = 'PointNet++ training on LFW-Reconst3D-MICA \nVerification (1:1)'
     subtitle = 'Parameters: ' + break_string(parameters, substring=', ')
     path_image = '/'.join(args.input_path.split('/')[:-1]) + '/training_history_from_log_file.png'
-    plot_training_history_pointnet2(epoch, eval_mean_loss, eval_accuracy, eval_avg_class_acc, title=title, subtitle=subtitle, path_image=path_image, show_fig=False, save_fig=True)
+    plot_training_history_pointnet2_verif_pairs(epoch, eval_mean_loss, eval_accuracy, title=title, subtitle=subtitle, path_image=path_image, show_fig=False, save_fig=True)
 
     
